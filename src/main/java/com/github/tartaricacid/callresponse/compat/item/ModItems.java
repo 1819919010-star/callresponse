9becefa84def3e5752fd40fb8ba425414d4a0c18
gly091020 <gulangyu1020@qq.com>
2026-07-30 22:27:01 +0800

    数值修改器发光

--- src/main/java/com/github/tartaricacid/callresponse/compat/item/ModItems.java
+++ src/main/java/com/github/tartaricacid/callresponse/compat/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.github.tartaricacid.callresponse.compat.item;
 
 import com.github.tartaricacid.callresponse.CallResponseMod;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Item;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -11,5 +12,5 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(BuiltInRegistries.ITEM, CallResponseMod.MOD_ID);
 
     public static final Supplier<Item> EMOTION_BOOK = ITEMS.register("emotion_book",
-            () -> new Item(new Item.Properties().stacksTo(1)));
+            () -> new Item(new Item.Properties().stacksTo(1).component(DataComponents.ENCHANTMENT_GLINT_OVERRIDE, true)));
 }
